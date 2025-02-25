f4c06c73fc50b4b4ebe0fb6b94528c7faf079392
kurzynamarcin <mail.phy6@gmail.com>
2025-02-25 19:38:48 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Point p1 = new Point();
         p1.x = 5;
         p1.y = 6;
-        System.out.println("Twoj x to " + p1.x + " a Twoj y to " + p1.y);
+        System.out.println(p1.toSvg());
 
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,20 @@
 public class Point {
     public double x;
     public double y;
+
+    public String toString(){
+        return "x=" + x + " y=" + y;
+    }
+    public String toSvg(){
+        return "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <circle r=\"45\" cx=\"" + x +"\" cy=\"" + y+ "\" fill=\"red\" />\n" +
+                "</svg>";
+    }
+
+
+
+
+
+
+
 }
