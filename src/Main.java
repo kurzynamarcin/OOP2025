c464103b8f3634ebbf66170c8fc9661699068c7c
kurzynamarcin <mail.phy6@gmail.com>
2025-02-25 18:52:48 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Point p1 = new Point();
+        p1.x = 5;
+        p1.y = 6;
+        System.out.println("Twoj x to " + p1.x + " a Twoj y to " + p1.y);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
